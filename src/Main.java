8c3cdc565184f81a0a7b097a26fb9287a68c62cf
RuLiberty <lifeup@inbox.ru>
2018-12-24 21:15:25 +0300

    Ex 1 and 2

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,17 @@
 public class Main {
+
+    public static void main(String[] args) {
+
+        byte a = 8;
+        short b = 32500;
+        int с = 256548475;
+        long d = 1234567891011121314L;
+
+        float e = 150.2f;
+        double f = 15.214584;
+
+        char g = 'A';
+        String h = "Hello World";
+
+    }
 }
