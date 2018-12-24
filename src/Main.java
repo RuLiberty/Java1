5414c038d53ca6f8f83c6b45307798c58f39fa1d
RuLiberty <lifeup@inbox.ru>
2018-12-24 21:46:24 +0300

    Ex. 4

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,21 @@ public class Main {
         char g = 'A'; // инициализация char
         String h = "Hello World"; // инициализация String
 
+        boolean bool = true; // инициализация Boolean
+
+        int aq = 5;
+
+        int bq = 10;
+
         System.out.println(FirstMetod(a,b,c,d));
+
+        // проверка работы метода
+        System.out.println(isDot(a,b)); // 32508 не входит в диапазон (false)
+        System.out.println(isDot(aq,bq)); // 15 входит в диапазон (true)
+        System.out.println(isDot(aq,aq)); // 10 входит в диапозон (true)
+
+
+
 
     }
 
@@ -21,5 +35,16 @@ public class Main {
     public static int FirstMetod (byte a, short b, int c, long d)
     {
         return (int) (a * (b + (c / d)));
+    }
+
+    // метод, принимающий на вход два числа, и проверяющий что их сумма лежит в пределах от 10 до 20(включительно), если да – вернуть true, в противном случае – false;
+    public static boolean isDot(int a, int b)
+    {
+        boolean p = false;
+        int с = a + b;
+        if ((с >= 10) & (с <= 20)) {
+            p = true;
+        }
+        return p;
     }
 }
