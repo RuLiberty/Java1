c772c454a8eddb20bb00b4e34c2bd3817418e3fe
RuLiberty <lifeup@inbox.ru>
2018-12-25 13:30:00 +0300

    Final HomeWork #1

--- src/Main.java
+++ src/Main.java
@@ -25,7 +25,7 @@ public class Main {
         int bq = 10;
 
         // задание 3
-        System.out.println(FirstMetod(a,b,c,d));
+        System.out.println(getResult(a,b,c,d));
 
         // задание 4. проверка работы метода
         System.out.println(isDot(a,b)); // 32508 не входит в диапазон (false)
@@ -33,72 +33,47 @@ public class Main {
         System.out.println(isDot(aq,aq)); // 10 входит в диапозон (true)
 
         // задание 5
-        myOut(a); // положительное число
-        myOut(otrA); // отрицательное число
-        myOut(zero); // ноль
+        getTypeNum(a); // положительное число
+        getTypeNum(otrA); // отрицательное число
+        getTypeNum(zero); // ноль
 
         // задание 6
         System.out.println(isNegative(a)); // false
         System.out.println(isNegative(otrA)); // true
 
         // задание 7
-        Hello(myName);
+        getHello(myName);
 
         // задание 8
-        visocos(1943); // не вискосный
-        visocos(1904); // високосный
-        visocos(1900); // не вискосный
-        visocos(2000); // високосный
-        visocos(1868); // високосный
+        getVisocos(1943); // не вискосный
+        getVisocos(1904); // високосный
+        getVisocos(1900); // не вискосный
+        getVisocos(2000); // високосный
+        getVisocos(1868); // високосный
     }
 
     // метод вычисляющий выражение a * (b + (c / d)) и возвращающий результат,где a, b, c, d – входные параметры этого метода
-    public static int FirstMetod (byte a, short b, int c, long d)
-    {
-        return (int) (a * (b + (c / d)));
-    }
+    public static int getResult (byte a, short b, int c, long d) {return (int) (a * (b + (c / d)));}
 
     // метод, принимающий на вход два числа, и проверяющий что их сумма лежит в пределах от 10 до 20(включительно), если да – вернуть true, в противном случае – false;
-    public static boolean isDot(int a, int b)
-    {
-        boolean p = false;
-        int с = a + b;
-        if ((с >= 10) && (с <= 20)) {
-            p = true;
-        }
-        return p;
-    }
+    public static boolean isDot(int a, int b) {return (a + b >= 10) && (a + b <= 20);}
 
     /* метод, которому в качестве параметра передается целое число, метод должен напечатать в консоль положительное ли число передали, или отрицательное;
-    Замечание: ноль считаем положительным числом.
-     */
-    public static void myOut (int a) {
-        if (a < 0)
-        {
-            System.out.println(a + " - Это отрицательное число");
-        } else {
-            System.out.println(a + " - Это или положительное число или 0");
-        }
-    }
+    Замечание: ноль считаем положительным числом. */
+    public static void getTypeNum (int a) {
+        if (a < 0) { System.out.println(a + " - Это отрицательное число");} else {System.out.println(a + " - Это или положительное число или 0");}}
 
     // метод, которому в качестве параметра передается целое число, метод должен вернуть true, если число отрицательное;
-    public static boolean isNegative(int a) {
-        if (a < 0) {
-            return true;
-        } else {
-            return false;
-        }
-    }
+    public static boolean isNegative(int a) { return a < 0;}
 
     // метод, которому в качестве параметра передается строка, обозначающая имя, метод должен вывести в консоль сообщение «Привет, указанное_имя!»
-    public static void Hello(String name) {
-        System.out.println("Привет, " + name + "!");
+    public static void getHello(String name) {System.out.println("Привет, " + name + "!");
     }
 
     // метод, который определяет является ли год високосным, и выводит сообщение в консоль.
     // Каждый 4-й год является високосным, кроме каждого 100-го, при этом каждый 400-й – високосный
-    public static void visocos(int year) {
-        if (((year%400) == 0) || ((((year%100) != 0)) && (((year%4) == 0)))) {
+    public static void getVisocos(int year) {
+        if ((year%400 == 0) || (year%100 != 0) && (year%4 == 0)) {
             System.out.println(year + " - этот год високосный");
         }
         else {
