a1ffbcca703b2b26d1d94e05b7bd7bbd23dccb80
RuLiberty <lifeup@inbox.ru>
2019-01-13 22:55:38 +0300

    exp.1 lesson2

--- src/Lesson2/Lesson2.java
+++ src/Lesson2/Lesson2.java
@@ -1,11 +1,35 @@
 package Lesson2;
 
+import java.util.Arrays;
+
 public class Lesson2 {
     public static void main(String[] args) {
 
-        byte[] firstArray = new byte[8];
+        int[] firstArray = new int[]{1,0,1,0,1,1,1,1};
 
+
+
+        changeArr(firstArray);
 
     }
 
+    // Задание №1
+    public static void changeArr(int[] b)
+    {
+        // Вывод искодного массива:
+        System.out.print("Исходный массив:  ");
+        System.out.println(Arrays.toString(b));
+
+        //С помощью цикла и условия заменим 0 на 1, 1 на 0
+        for (int i = 0; i < b.length; i++)
+        {
+            if (b[i] == 1) {b[i] = 0;}
+            else {b[i] = 1;}
+        }
+
+        // Вывод результата
+        System.out.print("Результат работы: ");
+        System.out.println(Arrays.toString(b));
+        System.out.println("");
+    }
 }
